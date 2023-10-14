dddb3ed43179179b36538b2e6f40f5d4f81cb942
Bikram Debanth <bikram@Bikrams-MacBook-Pro.local>
2023-10-15 02:15:05 +0530

    Test Git

---
+++ src/main/datastrucure/Test.java
@@ -0,0 +1,10 @@
+package src.main.datastrucure;
+
+public class Test {
+    /**
+     * Test Main
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
